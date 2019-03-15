f90e262a6a947ed511247a38f62d330f136ee9d6
freezer974 <tony_chevalier@hotmail.com>
2019-03-15 17:49:45 +0400

    vue galerie & lightbox

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,4 +6,5 @@ try {
     window.$ = window.jQuery = require('jquery');
     require('bootstrap');
     require('@fortawesome/fontawesome-free/js/all.js');
+    require('magnific-popup');
 } catch (e) {}
