2a0ccdd38392f5324fbf75b446b57c38468373a7
freezer974 <tony_chevalier@hotmail.com>
2019-03-15 22:56:37 +0400

    modif

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::middleware('admin')->group(function () {
     ]);
 });
 
-Route::middleware ('auth', 'verified')->group (function () {
+Route::middleware ('auth', 'verified')->group(function () {
 
     Route::resource('image', 'ImageController', [
         'only' => ['create', 'store', 'destroy', 'update']
