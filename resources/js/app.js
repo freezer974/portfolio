afab92ef1bfd5bf9befd4069682f727378074619
freezer974 <tony_chevalier@hotmail.com>
2019-03-15 09:09:22 +0400

    add categories - admin panel

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,4 +3,5 @@ window.Popper = require('popper.js').default;
 try {
     window.$ = window.jQuery = require('jquery');
     require('bootstrap');
+    require('@fortawesome/fontawesome-free/js/all.js');
 } catch (e) {}
