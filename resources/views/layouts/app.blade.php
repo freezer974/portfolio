13e80df4575b4377d9907e407631c734e276970f
freezer974 <tony_chevalier@hotmail.com>
2019-03-16 10:18:22 +0400

    gestion utilisateur et mode maintenance

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,7 +53,10 @@
                 <a class="nav-link dropdown-toggle{{ currentRoute(
                                         route('category.create'),
                                         route('category.index'),
-                                        route('category.edit', request()->category?: 0)
+                                        route('category.edit', request()->category?: 0),
+                                        route('orphans.index'),
+                                        route('maintenance.index'),
+                                        route('user.index')
                                     )}}" href="#" id="navbarDropdownGestCat" role="button" data-toggle="dropdown"
                     aria-haspopup="true" aria-expanded="false">
                     @lang('Administration')
@@ -64,6 +67,15 @@
                     </a>
                     <a class="dropdown-item" href="{{ route('category.index') }}">
                         <i class="fas fa-wrench fa-lg"></i> @lang('Gérer les catégories')
+                    </a>
+                    <a class="dropdown-item" href="{{ route('orphans.index') }}">
+                        <i class="fas fa-images fa-lg"></i> @lang('Photos orphelines')
+                    </a>
+                    <a class="dropdown-item" href="{{ route('maintenance.index') }}">
+                        <i class="fas fa-cogs fa-lg"></i> @lang('Mode maintenance')
+                    </a>
+                    <a class="dropdown-item" href="{{ route('user.index') }}">
+                        <i class="fas fa-users fa-lg"></i> @lang('Utilisateurs')
                     </a>
                 </div>
             </li>
@@ -94,24 +106,32 @@
             @endauth
         </ul>
         <ul class="navbar-nav ml-auto">
-            @guest
+                @guest
                 <li class="nav-item{{ currentRoute(route('login')) }}"><a class="nav-link" href="{{ route('login') }}">@lang('Connexion')</a></li>
                 <li class="nav-item{{ currentRoute(route('register')) }}"><a class="nav-link" href="{{ route('register') }}">@lang('Inscription')</a></li>
-            @else
-                <li class="nav-item{{ currentRoute(
-                    route('profile.edit', auth()->id()),
-                    route('profile.show', auth()->id())
-                )}}">
-                    <a class="nav-link" href="{{ route('profile.edit', auth()->id()) }}">@lang('Profil')</a>
-                </li>
-                <li class="nav-item">
-                    <a id="logout" class="nav-link" href="{{ route('logout') }}">@lang('Déconnexion')</a>
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hide">
-                        {{ csrf_field() }}
-                    </form>
-                </li>
-            @endguest
-        </ul>
+                @else
+                    @maintenance
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('maintenance.index') }}" data-toggle="tooltip" title="@lang('Mode maintenance')">
+                                <span class="fas fa-exclamation-circle  fa-lg" style="color: red;">
+                                </span>
+                            </a>
+                        </li>
+                    @endmaintenance
+                    <li class="nav-item{{ currentRoute(
+                                route('profile.edit', auth()->id()),
+                                route('profile.show', auth()->id())
+                            )}}">
+                        <a class="nav-link" href="{{ route('profile.edit', auth()->id()) }}">@lang('Profil')</a>
+                    </li>
+                    <li class="nav-item">
+                        <a id="logout" class="nav-link" href="{{ route('logout') }}">@lang('Déconnexion')</a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hide">
+                            {{ csrf_field() }}
+                        </form>
+                    </li>
+                @endguest
+            </ul>
     </div>
 </nav>
 @if (session('ok'))
