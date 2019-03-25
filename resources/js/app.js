d0b4b75b5364e011afa29b0f1c4283a2941673b0
freezer974 <tony_chevalier@hotmail.com>
2019-03-25 07:57:16 +0400

    accueil javascript

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,19 @@
 window._ = require('lodash');
-window.Popper = require('popper.js').default;
 window.swal = require('sweetalert2');
+window.Popper = require('popper.js').default;
 
 try {
     window.$ = window.jQuery = require('jquery');
+    require('isotope-layout/dist/isotope.pkgd.min.js');
     require('bootstrap');
-    require('@fortawesome/fontawesome-free/js/all.js');
     require('magnific-popup');
+    require('@fortawesome/fontawesome-free/js/all.js');
     require('jquery-animated-headlines');
-    require('waypoints');
-    require('counterup');
+    require('waypoints/lib/jquery.waypoints.min.js');
+    require('counterup/jquery.counterup.min.js');
+    require('jquery.easing');
+    require('startbootstrap-scrolling-nav/js/scrolling-nav.js');
+    require('imagesloaded');
+    require('scrollup');
 
 } catch (e) {}
