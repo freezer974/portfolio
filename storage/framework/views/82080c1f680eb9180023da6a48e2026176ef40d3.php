4b55612d28155a74485deda8b1bafd0023f56787
CHEVALIER Tony <tony@tony974.net>
2019-03-27 05:22:24 +0100

    modif seed

---
+++ storage/framework/views/82080c1f680eb9180023da6a48e2026176ef40d3.php
@@ -0,0 +1,12 @@
+<div class="card text-white bg-dark mb-3">
+    <h4 class="card-header">
+        <?php echo e($title); ?>
+
+    </h4>
+    <div class="card-body">
+        <?php echo e($slot); ?>
+
+    </div>
+</div>
+
+<?php /* /homepages/25/d350060631/htdocs/tatoumicreation/resources/views/components/card.blade.php */ ?>
