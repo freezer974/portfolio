5d79352f6cc1856b665bb3f0daec277e38457188
CHEVALIER Tony <tony@tony974.net>
2019-03-27 12:38:57 +0100

    re

--- storage/framework/views/272858a16110787d339106a6c1b2f95f03149079.php
+++
@@ -1,6 +0,0 @@
-<button type="submit" class="btn <?php if(isset($color)): ?><?php echo e(' btn-' . $color); ?><?php else: ?> btn-primary <?php endif; ?> float-right">
-    <?php echo e($slot); ?>
-
-</button>
-
-<?php /* /homepages/25/d350060631/htdocs/tatoumicreation/resources/views/components/button.blade.php */ ?>
--- storage/framework/views/b094761fca45a0d312d022ce76ad8adb770feebe.php
+++ storage/framework/views/b094761fca45a0d312d022ce76ad8adb770feebe.php
@@ -363,9 +363,14 @@
                                 <a href="<?php echo e(url('images/' . $image->name)); ?>" class="image-link" data-link="<?php echo e(route('image.click', $image->id)); ?>"><img src="<?php echo e(url('thumbs/' . $image->name)); ?>" alt="image"></a>
                             </div>
                             <div class="portfolio-content">
-                                <a href="<?php echo e(url('images/' . $image->name)); ?>"><h4><?php echo e($image->description); ?></h4></a>
+                                <?php if(isset($image->title)): ?>
+                                    <a href="<?php echo e($image->url); ?>"><h4 class="text-dark"><?php echo e($image->title); ?></h4></a>
+                                <?php endif; ?>
                                 <?php if(isset($image->description)): ?>
-                                        <p class="text-muted"><?php echo e($image->description); ?></p>
+                                    <span class="text-muted"><?php echo e($image->description); ?></span>
+                                <?php endif; ?>
+                                <?php if(isset($image->url)): ?>
+                                    <em class="d-block"><a href="<?php echo e($image->url); ?>">Site web</a></em>
                                 <?php endif; ?>
                                 <em>fait par <a href="<?php echo e(route('user', $image->user->id)); ?>" data-toggle="tooltip" title="<?php echo e(__('Voir les photos de ') . $image->user->name); ?>"><?php echo e($image->user->name); ?></a></em>
                                 <div class="float-right">
@@ -546,8 +551,6 @@
 
             $('.site-wrapper').fadeOut(1000)
 
-            $('[data-toggle="tooltip"]').tooltip()
-
             $('a.toggleIcons').click((e) => {
                 e.preventDefault();
                 let that = $(e.currentTarget)
@@ -574,14 +577,7 @@
             $('.clip').animatedHeadline()
         })
 
-        $(window).on('scroll', function () {
-            var scroll = $(window).scrollTop();
-            if (scroll < 15) {
-                $("nav.navbar").removeClass("scroll-header");
-            } else {
-                $("nav.navbar").addClass("scroll-header");
-            }
-        });
+
 
             const swallAlertServer = () => {
                 swal.fire({
