56177c073a75c9f166d1e427e5ff7ed6eb4f31c9
freezer974 <tony_chevalier@hotmail.com>
2019-03-30 14:27:30 +0400

    zoom image - condition pour ajout site web

--- storage/framework/views/e2155bfba19e9fd3cac68b905e09c54498837256.php
+++
@@ -1,7 +0,0 @@
-<?php /* /Users/tonychevalier/sites/simplon/heroku/tatoumi/resources/views/components/checkbox.blade.php */ ?>
-<div class="form-group">
-    <div class="custom-control custom-checkbox">
-        <input type="checkbox" class="custom-control-input" id="<?php echo e($name); ?>" name="<?php echo e($name); ?>" <?php if($checked): ?> checked <?php endif; ?>>
-        <label class="custom-control-label" for="<?php echo e($name); ?>"> <?php echo e($label); ?></label>
-    </div>
-</div>
