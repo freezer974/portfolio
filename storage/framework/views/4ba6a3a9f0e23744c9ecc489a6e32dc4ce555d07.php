fcf901e72d92b99b75f5de1655ae5a387f808291
freezer974 <tony_chevalier@hotmail.com>
2019-03-31 15:58:04 +0400

    mailer

---
+++ storage/framework/views/4ba6a3a9f0e23744c9ecc489a6e32dc4ce555d07.php
@@ -0,0 +1,15 @@
+<?php /* /Users/tonychevalier/sites/simplon/heroku/tatoumi/resources/views/email.blade.php */ ?>
+Vous avez un message de : <?php echo e($name); ?>
+
+<p>
+Nom: <?php echo e($name); ?>
+
+</p>
+<p>
+Email: <?php echo e($email); ?>
+
+</p>
+<p>
+Message: <?php echo e($user_message); ?>
+
+</p>
