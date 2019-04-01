2b7281705c38ffda499237473ecc0d2845f60a1d
freezer974 <tony_chevalier@hotmail.com>
2019-04-01 10:51:07 +0400

    amélioration formulaire contact

--- resources/views/partials/form-group.blade.php
+++ resources/views/partials/form-group.blade.php
@@ -1,6 +1,6 @@
 <div class="form-group {{ $class ?? '' }}">
     <label for="{{ $name }}">{{ $title }}</label>
-    <input id="{{ $name }}" type="{{ $type }}" class="form-control{{ $errors->has($name) ? ' is-invalid' : '' }}" name="{{ $name }}" value="{{ old($name, isset($value) ? $value : '') }}" {{ $required ? 'required' : ''}}>
+    <input id="{{ $name }}" type="{{ $type }}" class="form-control {{ $errors->has($name) ? ' is-invalid' : '' }}" placeholder="{{ $placeholder ?? '' }}" name="{{ $name }}" value="{{ old($name, isset($value) ? $value : '') }}" {{ $required ? 'required' : ''}}>
     @if ($errors->has($name))
         <div class="invalid-feedback">
             {{ $errors->first($name) }}
