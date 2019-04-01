1be830c7f61f17f158b4499e428b0acf30194f90
freezer974 <tony_chevalier@hotmail.com>
2019-04-01 12:58:09 +0400

    validation formulaire contact

--- resources/views/partials/textarea-form-group.blade.php
+++ resources/views/partials/textarea-form-group.blade.php
@@ -1,6 +1,6 @@
 <div class="form-group {{ $class ?? '' }}">
     <label for="{{ $name }}">{{ $title }}</label>
-    <textarea class="form-control" id="{{ $name }}" rows="{{ $rows ? $rows : '3' }}" placeholder="{{ $placeholder ? $placeholder : '' }}"></textarea>
+    <textarea class="form-control {{ $errors->has($name) ? ' is-invalid' : '' }}" id="{{ $name }}" rows="{{ $rows ? $rows : '3' }}" placeholder="{{ $placeholder ? $placeholder : '' }}" name="{{ $name }}" min="3" max="1000" >{{ old($name, isset($value) ? $value : '') }}</textarea>
     @if ($errors->has($name))
         <div class="invalid-feedback">
             {{ $errors->first($name) }}
