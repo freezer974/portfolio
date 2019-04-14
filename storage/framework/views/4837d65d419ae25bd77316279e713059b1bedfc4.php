ce58649bab3f0debf218ce1dd119272559d67cf4
freezer974 <tony_chevalier@hotmail.com>
2019-04-14 20:54:09 +0400

    ajustement

--- storage/framework/views/4837d65d419ae25bd77316279e713059b1bedfc4.php
+++
@@ -1,9 +0,0 @@
-<input id="<?php echo e($name); ?>" type="hidden" class="form-control <?php echo e($errors->has($name) ? ' is-invalid' : ''); ?>" name="<?php echo e($name); ?>" <?php echo e($required ? 'required' : ''); ?>>
-<?php if($errors->has($name)): ?>
-    <div class="invalid-feedback mb-3">
-        <?php echo e($errors->first($name)); ?>
-
-    </div>
-<?php endif; ?>
-
-<?php /* /Users/tonychevalier/sites/tatoumicreation/resources/views/partials/recaptcha-hidden.blade.php */ ?>
